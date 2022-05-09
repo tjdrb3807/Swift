1d486d8ac97cc54220df672882ebcc198d1b77af
tjdrb3807 <junsung3807@naver.com>
2022-05-09 17:27:05 +0900

    ...

---
+++ SideProject/Diary/Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by 전성규 on 2022/05/09.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ SideProject/Diary/Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 전성규 on 2022/05/09.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
