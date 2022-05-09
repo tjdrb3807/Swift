1e26930e821b29804aee45b15fc0efd888ee595c
tjdrb3807 <junsung3807@naver.com>
2022-05-09 17:48:56 +0900

    ...

---
+++ SideProject/Diary/Diary/WriteDiaryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 전성규 on 2022/05/09.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
