e7062f9ce546f973179fe72cd141e0f993833753
tjdrb3807 <junsung3807@naver.com>
2022-05-09 16:26:26 +0900

    ...

---
+++ SideProject/Diary/Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by 전성규 on 2022/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
