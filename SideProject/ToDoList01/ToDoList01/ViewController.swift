ae1c152df049e94e8750189fbc0150602fd8f5d1
tjdrb3807 <junsung3807@naver.com>
2022-05-12 12:35:04 +0900

    ...

---
+++ SideProject/ToDoList01/ToDoList01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList01
+//
+//  Created by 전성규 on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
