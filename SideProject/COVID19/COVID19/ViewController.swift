6f97d152b8d4f8c7dade53338bd1a73742d730c9
tjdrb3807 <junsung3807@naver.com>
2022-05-16 20:13:52 +0900

    ...

---
+++ SideProject/COVID19/COVID19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 전성규 on 2022/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
